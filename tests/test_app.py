136747776a5e856c46242b92c11f649096698437
sedoley <71331799+sedoley@users.noreply.github.com>
2026-05-11 03:50:32 +0000

    test(ci): improve utility for commit #1817

--- tests/test_app.py
+++ tests/test_app.py
@@ -989,3 +989,7 @@ def helper_159():
 # Helper added in commit #1767
 def helper_167():
     return "real-change-1767"
+
+# Helper added in commit #1817
+def helper_17():
+    return "real-change-1817"
