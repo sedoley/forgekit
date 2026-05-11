9abd68addacc9f70d0d9d9e7dbc2f3d24f933d09
sedoley <71331799+sedoley@users.noreply.github.com>
2026-05-11 03:58:39 +0000

    style: improve utility for commit #6597

--- utils/helpers.py
+++ utils/helpers.py
@@ -3759,3 +3759,7 @@ def helper_185():
 # Helper added in commit #6588
 def helper_188():
     return "real-change-6588"
+
+# Helper added in commit #6597
+def helper_197():
+    return "real-change-6597"
